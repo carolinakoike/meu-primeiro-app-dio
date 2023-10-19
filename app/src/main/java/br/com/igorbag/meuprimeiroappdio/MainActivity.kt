96c0c97d905c673c8306e137e7a18c346ae421f5
carolinakoike <karolkoike@hotmail.com>
2023-10-19 18:41:57 -0300

    mudanças no app

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -2,13 +2,15 @@ package br.com.igorbag.meuprimeiroappdio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = getString(R.string.hello_world)
     }
 }
